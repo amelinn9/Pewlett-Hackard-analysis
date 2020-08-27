0b0640b807850ea3a2920d34b0ef1c385ec0fce2
amelin <anorzamini@gmail.com>
2020-08-26 17:29:36 -0700

    adding schema file

--- schema.sql
+++ schema.sql
@@ -1,11 +1,11 @@
--- creating tables for PH-EmployeeDB
+-- Creating tables for PH-EmployeeDB
 CREATE TABLE departments (
-	dept_no VARCHAR(4) NOT NULL,
-	dept_name VARCHAR(40) NOT NULL,
-	PRIMARY KEY (dept_no),
-	UNIQUE (dept_name)
+     dept_no VARCHAR(4) NOT NULL,
+     dept_name VARCHAR(40) NOT NULL,
+     PRIMARY KEY (dept_no),
+     UNIQUE (dept_name)
 );
-	
+
 CREATE TABLE employees (
 	emp_no INT NOT NULL,
 	birth_date DATE NOT NULL,
@@ -29,17 +29,17 @@ CREATE TABLE dept_manager (
 CREATE TABLE salaries (
 	emp_no INT NOT NULL,
 	salary INT NOT NULL,
-	from_date DATE,
-	to_date DATE,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	PRIMARY KEY (emp_no)
 );
 
-CREATE TABLE dept_employee (
+CREATE TABLE dept_emp (
 	dept_no VARCHAR(4) NOT NULL,
 	emp_no INT NOT NULL,
-	from_date DATE,
-	to_date DATE,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	PRIMARY KEY (dept_no, emp_no)
@@ -48,8 +48,7 @@ CREATE TABLE dept_employee (
 CREATE TABLE titles (
 	emp_no INT NOT NULL,
 	title VARCHAR NOT NULL,
-	from_date DATE,
-	to_date DATE,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
